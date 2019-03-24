de6353543884cec0b07a6b2797f01af2ee277eac
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-03-24 17:15:56 -0300

    Versão final Design patterns Template Method

--- src/estruturado/GeradorArquivo.java
+++ src/estruturado/GeradorArquivo.java
@@ -1,76 +1,29 @@
 package estruturado;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
-public class GeradorArquivo {
+public abstract class GeradorArquivo {
 	
-	public final void gerarArquivo(String nome, Map<String,Object> propriedades, String tipo) throws IOException {
-		byte[] bytes = null;
-		if (tipo.equals("PROPRIEDADES_CRIPTOGRAFADO")) {
-			bytes = processaPropriedadesCriptografado(propriedades);
-		} else if (tipo.equals("XML_COMPACTADO")) {
-			//gera xml
-			bytes = porcessaXmlCompactado(propriedades);
-		} else {
-			System.out.println("Desconheço essa opção");
-		}
+	public final void gerarArquivo(String nome, Map<String,Object> propriedades) throws IOException {
+		
+		String conteudo = gerarConteudo(propriedades);
+		byte[] bytes = processaConteudo(conteudo.getBytes()); 
 		FileOutputStream fileout = new FileOutputStream(nome);
 		fileout.write(bytes);
 		fileout.close();
 	}
 
-	private byte[] porcessaXmlCompactado(Map<String,Object> propriedades) throws IOException {
-	String conteudo = geraConteudoXml(propriedades);
-	return porcessaCompactacao(conteudo.getBytes());
-	}
+   protected abstract String gerarConteudo(Map<String,Object> propriedades);
+	 
+	 
+   protected byte[] processaConteudo(byte[] conteudo ) throws IOException {
+
+	   return conteudo;
+   }
+ 
+ 
 	
-	private byte[] processaPropriedadesCriptografado(Map<String,Object> propriedades) {
-		String conteudo = geraConteudoPropriedades(propriedades);
-		return processaCriptografado(conteudo.getBytes());
-	}
-	
-	private String geraConteudoXml(Map<String,Object> propriedades) {
-		//gera xml
-		StringBuilder propFileBuilder = new StringBuilder();
-		propFileBuilder.append("<properties>");
-		for (String prop: propriedades.keySet()) {
-		propFileBuilder.append("<"+prop+">"+propriedades.get(prop)+"</"+prop+">");
-		}
-		propFileBuilder.append("</propriedades>");
-		return propFileBuilder.toString();
-	}
-	
-	private byte[] porcessaCompactacao(byte[] bytes) throws IOException {
-		//compacta
-	    ByteArrayOutputStream byteout = new ByteArrayOutputStream();
-		ZipOutputStream out = new ZipOutputStream(byteout);
-		out.putNextEntry(new ZipEntry("internal"));
-		out.write(bytes);
-		out.closeEntry();
-		out.close();
-		return byteout.toByteArray();
-	}
-	
-	private byte[] processaCriptografado(byte[] bytes) {
-		byte[] newBytes = new byte[bytes.length];
-		for (int i = 0; i<bytes.length;i++) {
-			newBytes[i] = (byte)((bytes[i]+10) % Byte.MAX_VALUE);
-		}
-		return newBytes; 
-	}
-	
-	private String geraConteudoPropriedades(Map<String,Object> propriedades) {
-		//gera properties
-		StringBuilder propFileBuilder = new StringBuilder();
-		for (String prop: propriedades.keySet()) {
-		propFileBuilder.append(prop+"m"+propriedades.get(prop)+"\n");
-		}
-		return propFileBuilder.toString();
-	}
 		
 }
--- src/estruturado/Principal.java
+++ src/estruturado/Principal.java
@@ -10,11 +10,12 @@ public class Principal {
 		mapa.put("nome", "Diego Henrique Silva");
 		mapa.put("idade", 30);
 		
-		GeradorArquivo gerador = new GeradorArquivo();
-		gerador.gerarArquivo("c:\\Gerador\\Cripto.txt", mapa, "PROPRIEDADES_CRIPTOGRAFADO");
+		GeradorArquivo gerador = new GeradorArquivoPropriedadesCriptografado();
+		gerador.gerarArquivo("c:\\Gerador\\Cripto.txt", mapa);
 		
-		GeradorArquivo gerador1 = new GeradorArquivo();
-		gerador1.gerarArquivo("c:\\Gerador\\XML.zip", mapa, "XML_COMPACTADO");		
+		GeradorArquivo gerador1 = new GeradorArquivoPropriedadesCriptografado();
+		gerador1.gerarArquivo("c:\\Gerador\\XML.zip", mapa);		
+			
 	}
 
 }
